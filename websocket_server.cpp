71bb104e2491b5b40f2042f57ae9ed56fd59f577
泽庭 邓 <w1248974412@126.com>
2024-03-07 00:11:30 +0800

    websocket_server.cpp

--- websocket_server.cpp
+++ websocket_server.cpp
@@ -1,34 +1,43 @@
-#include "websocket_server.h"
 #include "stdafx.h"
+#include "websocket_server.h"
+
 namespace websocket {
 
-    // session类的构造函数已在头文件中定义，这里我们实现start方法
+    session::session(tcp::socket socket) : ws_(std::move(socket)) {}
+
     void session::start() {
-        // 执行WebSocket握手
-        ws_.async_accept([self = shared_from_this()](boost::system::error_code ec) {
-            if (!ec) {
-                // 握手成功，可以在这里开始读写操作
-                self->do_read(); // 假设你有这样一个方法来开始读取操作
-            }
-            // 注意：实际代码中应处理错误情况
+        ws_.async_accept(
+            [self = shared_from_this()](boost::system::error_code ec) {
+                if (!ec) self->do_read();
+                // 处理错误情况
             });
     }
 
-    // 假设的do_read方法，用于开始读取数据
     void session::do_read() {
-        ws_.async_read(buffer_, [self = shared_from_this()](boost::system::error_code ec, std::size_t bytes_transferred) {
-            if (!ec) {
-                // 成功读取消息，可以在这里处理消息
-                // 然后继续读取下一个消息
-                self->do_read();
-            }
-            // 注意：实际代码中应处理错误情况
+        ws_.async_read(
+            buffer_,
+            [self = shared_from_this()](boost::system::error_code ec, std::size_t bytes_transferred) {
+                if (!ec) {
+                    // 这里处理接收到的消息
+                    self->do_read(); // 继续读取下一个消息
+                }
+                // 处理错误情况
             });
     }
 
-    // server类构造函数和run方法已在头文件中声明，这里实现它们
-    server::server(boost::asio::io_context& ioc, unsigned short port)
-        : ioc_(ioc), acceptor_(ioc, tcp::endpoint(tcp::v4(), port)) {
+    void session::do_write(std::string message) {
+        auto self = shared_from_this();
+        ws_.async_write(
+            boost::asio::buffer(message),
+            [self](boost::system::error_code ec, std::size_t length) {
+                if (!ec) {
+                    // 消息发送成功后的处理
+                }
+                // 处理错误情况
+            });
+    }
+
+    server::server(boost::asio::io_context& ioc, unsigned short port) : ioc_(ioc), acceptor_(ioc, tcp::endpoint(tcp::v4(), port)) {
         do_accept();
     }
 
@@ -37,13 +46,12 @@ namespace websocket {
     }
 
     void server::do_accept() {
-        acceptor_.async_accept([this](boost::system::error_code ec, tcp::socket socket) {
-            if (!ec) {
-                // 成功接受新连接
-                std::make_shared<session>(std::move(socket))->start();
-            }
-            // 继续接受下一个连接
-            do_accept();
+        acceptor_.async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket) {
+                if (!ec) {
+                    std::make_shared<session>(std::move(socket))->start();
+                }
+                do_accept(); // 继续接受新的连接
             });
     }
 
--- websocket_server.h
+++ websocket_server.h
@@ -1,55 +1,37 @@
-// websocket_server.h
 #ifndef WEBSOCKET_SERVER_H
 #define WEBSOCKET_SERVER_H
-
-#include "stdafx.h"
 
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
 #include <memory>
+#include <string>
 
 namespace websocket {
 
-    using tcp = boost::asio::ip::tcp;               // 使用类型别名简化类型名称
-    namespace websocket = boost::beast::websocket;  // 使用类型别名简化类型名称
+    using tcp = boost::asio::ip::tcp;               // Simplify the type name
+    namespace websocket = boost::beast::websocket;  // Simplify the type name
 
     class session : public std::enable_shared_from_this<session> {
     public:
-        explicit session(tcp::socket socket) : ws_(std::move(socket)) {}
+        explicit session(tcp::socket socket);
 
-        websocket::stream<tcp::socket> ws_;
-        boost::beast::flat_buffer buffer_;
-
-        void start() {
-            // 在这里执行WebSocket握手并开始读取或写入操作
-        }
+        void start();
+        void do_read();
+        void do_write(std::string message);
 
     private:
-        websocket::stream<tcp::socket> ws_; // WebSocket流
+        websocket::stream<tcp::socket> ws_;
+        boost::beast::flat_buffer buffer_;
     };
 
     class server {
     public:
-        server(boost::asio::io_context& ioc, unsigned short port)
-            : ioc_(ioc), acceptor_(ioc, tcp::endpoint(tcp::v4(), port)) {
-            do_accept();
-        }
+        server(boost::asio::io_context& ioc, unsigned short port);
 
-        void run() {
-            ioc_.run();
-        }
+        void run();
+        void do_accept();
 
     private:
-        void do_accept() {
-            acceptor_.async_accept([this](boost::system::error_code ec, tcp::socket socket) {
-                if (!ec) {
-                    // 成功接受连接后，创建一个新的session
-                    std::make_shared<session>(std::move(socket))->start();
-                }
-                do_accept(); // 继续接受新的连接
-                });
-        }
-
         boost::asio::io_context& ioc_;
         tcp::acceptor acceptor_;
     };
